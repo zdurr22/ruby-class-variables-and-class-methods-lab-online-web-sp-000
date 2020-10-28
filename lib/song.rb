b3fd17598657d33594f2dc732e814cc250171450
Zachary Durr <zdurr22@gmail.com>
2020-10-28 22:55:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+  @@count = 0
   @@artists = []
 
   def self.count
@@ -14,7 +14,7 @@ class Song
     @artist = artist
     @genre = genre
 
-    @@song_count += 1
+    @@count += 1
 
     @@artists << artist unless @@artists.include?(artist)
 
