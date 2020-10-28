5e2a33330460dcd8454df729bb029cc17130ab5a
Zachary Durr <zdurr22@gmail.com>
2020-10-28 22:54:02 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+  
+  @@song_count = 0
+  ARTISTS = []
+
+  def self.count
+    @@song_count
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@song_count += 1
+
+    ARTISTS << artist unless ARTISTS.include?(artist)
+
+  end
+
+
+
+
+
+end
