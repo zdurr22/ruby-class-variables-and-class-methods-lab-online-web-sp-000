1f0e38be0188e0144e45639727477159e1567d89
Zachary Durr <zdurr22@gmail.com>
2020-10-28 23:10:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,17 @@ class Song
     end
     genre_count
   end
+  
   def self.artist_count
-
+    artist_count = {}
+    @@artists.each do |artist|
+    if artist_count[artist] 
+        artist_count[artist] += 1
+      else 
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
   end
 
   def initialize(name, artist, genre)
